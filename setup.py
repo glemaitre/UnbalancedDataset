27fffea721fe1edafc50d1cfc65d1b857292b6c1
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-09-03 11:24:50 +0200

    EHN: add Hellinger criterion

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python
 """Toolbox for imbalanced dataset in machine learning."""
 
-import codecs
+import io
+import re
 import os
+import sys
+import subprocess
+import codecs
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
 
-# get __version__ from _version.py
-ver_file = os.path.join('imblearn', '_version.py')
-with open(ver_file) as f:
-    exec(f.read())
-
+PACKAGE_NAME = 'imblearn'
 DISTNAME = 'imbalanced-learn'
 DESCRIPTION = 'Toolbox for imbalanced dataset in machine learning.'
 with codecs.open('README.rst', encoding='utf-8-sig') as f:
@@ -20,7 +20,6 @@ MAINTAINER_EMAIL = 'g.lemaitre58@gmail.com, ichkoar@gmail.com'
 URL = 'https://github.com/scikit-learn-contrib/imbalanced-learn'
 LICENSE = 'MIT'
 DOWNLOAD_URL = 'https://github.com/scikit-learn-contrib/imbalanced-learn'
-VERSION = __version__
 INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn']
 CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Intended Audience :: Developers',
@@ -38,16 +37,88 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Programming Language :: Python :: 3.6']
 
 
-setup(name=DISTNAME,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      description=DESCRIPTION,
-      license=LICENSE,
-      url=URL,
-      version=VERSION,
-      download_url=DOWNLOAD_URL,
-      long_description=LONG_DESCRIPTION,
-      zip_safe=False,  # the package can run out of an .egg file
-      classifiers=CLASSIFIERS,
-      packages=find_packages(),
-      install_requires=INSTALL_REQUIRES)
+def version(package, encoding='utf-8'):
+    """Obtain the packge version from a python file e.g. pkg/_version.py
+    See <https://packaging.python.org/en/latest/single_source_version.html>.
+    """
+    path = os.path.join(os.path.dirname(__file__), package, '_version.py')
+    with io.open(path, encoding=encoding) as fp:
+        version_info = fp.read()
+    version_match = re.search(r"""^__version__ = ['"]([^'"]*)['"]""",
+                              version_info, re.M)
+    if not version_match:
+        raise RuntimeError("Unable to find version string.")
+    return version_match.group(1)
+
+
+def generate_cython(package):
+    """Cythonize all sources in the package"""
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    print("Cythonizing sources")
+    p = subprocess.call([sys.executable,
+                         os.path.join(cwd, 'tools', 'cythonize.py'),
+                         package],
+                        cwd=cwd)
+    if p != 0:
+        raise RuntimeError("Running cythonize failed!")
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+    config.set_options(ignore_setup_xxx_py=True,
+                       assume_default_configuration=True,
+                       delegate_options_to_subpackages=True,
+                       quiet=True)
+
+    config.add_subpackage(PACKAGE_NAME)
+
+    return config
+
+
+def setup_package():
+    from numpy.distutils.core import setup
+
+    old_path = os.getcwd()
+    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    src_path = local_path
+
+    os.chdir(local_path)
+    sys.path.insert(0, local_path)
+
+    # Run build
+    old_path = os.getcwd()
+    os.chdir(src_path)
+    sys.path.insert(0, src_path)
+
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    if not os.path.exists(os.path.join(cwd, 'PKG-INFO')):
+        # Generate Cython sources, unless building from source release
+        generate_cython(PACKAGE_NAME)
+
+    try:
+        setup(name=DISTNAME,
+              author=MAINTAINER,
+              author_email=MAINTAINER_EMAIL,
+              maintainer=MAINTAINER,
+              maintainer_email=MAINTAINER_EMAIL,
+              description=DESCRIPTION,
+              license=LICENSE,
+              url=URL,
+              version=version(PACKAGE_NAME),
+              download_url=DOWNLOAD_URL,
+              long_description=LONG_DESCRIPTION,
+              zip_safe=False,  # the package can run out of an .egg file
+              classifiers=CLASSIFIERS,
+              packages=find_packages(),
+              install_requires=INSTALL_REQUIRES,
+              configuration=configuration)
+    finally:
+        del sys.path[0]
+        os.chdir(old_path)
+
+    return
+
+
+if __name__ == '__main__':
+    setup_package()
